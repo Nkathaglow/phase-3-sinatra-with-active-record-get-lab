41c4a6896394c23ccd991ca81a61cb63e628f53c
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-12 23:51:55 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  get './bakers' do
+    "Hello world"
+  end
+
+
+  get '/bakeries' do
+    # Get all the bakeries from the database
+    # Send them back as a JSON array
+  end
 
 end
--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,3 +1,16 @@
 class Bakery < ActiveRecord::Base
   # add association macro here
+
+  def index
+    bakeries = Bakery.all
+    render json: bakeries
+  end
+
+  def show
+    bakery = Bakery.find(params[:id])
+    baked_goods = bakery.baked_goods
+    render json: bakery, include: { baked_goods: { only: [:name, :price] } }
+  end
+
+  
 end
